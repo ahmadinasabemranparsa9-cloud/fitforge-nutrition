7b250f45c945134c90b017641c8809d32cb726f0
Parsa Ahmadi Nasab Emran <ahmadinasabemranparsa9@gmail.com>
2026-09-23 13:04:47 -0400

    feat: add nutrition json-phase server

---
+++ src/server.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+
+const app = require("./app");
+
+const PORT = process.env.PORT || 4003;
+
+app.listen(PORT, () => {
+    console.log(
+        `FitForge Nutirion service running on port ${PORT}`
+    );
+});
